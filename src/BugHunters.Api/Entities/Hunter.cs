7c66f414775fe145940a87cdca347f6972856b62
Troels Mortensen <tromortensen@hotmail.com>
2024-04-12 11:32:21 +0200

    starting on Hunter entity

---
+++ src/BugHunters.Api/Entities/Hunter.cs
@@ -0,0 +1,7 @@
+﻿namespace BugHunters.Api.Entities;
+
+public record Hunter(HunterId Id, string Name, StudentNumber StudentNumber);
+
+public record HunterId(Guid Value);
+
+public record StudentNumber(string Value);

c31ec3730eb45826efc0fbf29a063244ba3bcfb0
TRMO <tromortensen@hotmail.com>
2024-08-26 15:44:31 +0200

    fix via id
    Took 2 minutes

--- src/BugHunters.Api/Entities/Values/Hunter/ViaId.cs
+++ src/BugHunters.Api/Entities/Values/Hunter/ViaId.cs
@@ -1,38 +1,42 @@
+using static BugHunters.Api.Common.Result.ResultExt;
+
 namespace BugHunters.Api.Entities.Values.Hunter;
 
 public record ViaId
 {
     public string Value { get; set; }
 
-    private ViaId(string value)
-        => Value = value;
+    private ViaId(string value) =>
+        Value = value;
 
-    public static Result<ViaId> FromString(string value)
-        => Create(Prepare(value));
+    public static Result<ViaId> FromString(string value) =>
+        Create(Prepare(value));
 
-    private static string Prepare(string value)
-        => value
+    private static string Prepare(string value) =>
+        value
             .ToLower()
             .Trim()
             .Replace("@via.dk", "")
             .Replace("@viauc.dk", "");
 
-    private static Result<ViaId> Create(string value)
-        => Result.StartValidation<ViaId>()
+    private static Result<ViaId> Create(string value) =>
+        StartValidation()
             .AssertThat(
                 () => IsStudentNumberOrTeacherInitials(value),
                 new ResultError("Hunter.ViaId", "Invalid Via ID format. Must be 6 digits, or 3-4 letters.")
             )
             .WithPayloadIfSuccess(() => new ViaId(value));
 
-    private static bool IsStudentNumberOrTeacherInitials(string value)
-        => IsSixDigits(value) || IsValidInitials(value);
+    private static bool IsStudentNumberOrTeacherInitials(string value) =>
+        IsSixDigits(value) || IsValidInitials(value);
 
-    private static bool IsValidInitials(string value)
-        => value.Length is >= 3 and <= 4 && value.All(char.IsLetter);
+    private static bool IsValidInitials(string value) =>
+        value.Length is >= 3 and <= 4 && value.All(char.IsLetter);
 
-    private static bool IsSixDigits(string value)
-        => value.Length == 6 && value.All(char.IsDigit);
-    
-    private ViaId(){} // EFC
+    private static bool IsSixDigits(string value) =>
+        value.Length == 6 && value.All(char.IsDigit);
+
+    private ViaId()
+    {
+    } // EFC
 }

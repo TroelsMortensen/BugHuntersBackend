1bc83d7a7d3ee948c743437eb176e70fa970bff3
TRMO <tromortensen@hotmail.com>
2024-08-26 15:44:39 +0200

    Fix DisplayName
    Took 8 seconds

--- src/BugHunters.Api/Entities/Values/Hunter/DisplayName.cs
+++ src/BugHunters.Api/Entities/Values/Hunter/DisplayName.cs
@@ -1,4 +1,6 @@
-﻿namespace BugHunters.Api.Entities.Values.Hunter;
+﻿using static BugHunters.Api.Common.Result.ResultExt;
+
+namespace BugHunters.Api.Entities.Values.Hunter;
 
 public record DisplayName
 {
@@ -7,21 +9,21 @@ public record DisplayName
     private DisplayName(string value)
         => Value = value;
 
-    public static Result<DisplayName> FromString(string value)
-        => Result.StartValidation<DisplayName>()
+    public static Result<DisplayName> FromString(string value) =>
+        StartValidation()
             .AssertThat(() => !IsEmpty(value), new ResultError("Hunter.Name", "Name cannot be empty."))
             .AssertThat(() => IsBelowMaxLength(value), new ResultError("Hunter.Name", "Name must be less than 20 characters."))
             .AssertThat(() => IsAboveMinLength(value), new ResultError("Hunter.Name", "Name must be more than 2 characters."))
             .WithPayloadIfSuccess(() => new DisplayName(value));
 
-    private static bool IsAboveMinLength(string value)
-        => value.Length >= 2;
+    private static bool IsAboveMinLength(string value) =>
+        value.Length >= 2;
 
-    private static bool IsEmpty(string value)
-        => !string.IsNullOrWhiteSpace(value);
+    private static bool IsEmpty(string value) =>
+        !string.IsNullOrWhiteSpace(value);
 
-    private static bool IsBelowMaxLength(string value)
-        => value.Length <= 20;
+    private static bool IsBelowMaxLength(string value) =>
+        value.Length <= 20;
 
     private DisplayName()
     {

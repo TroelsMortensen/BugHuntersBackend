6c76b39c02d382e5957bc43314740f7d05e2360c
TRMO <tromortensen@hotmail.com>
2024-08-28 15:42:15 +0200

    Update Change Display Name feature, now compiles. Will eventually test
    Took 22 seconds

--- src/BugHunters.Api/Features/ChangeDisplayName/ChangeDisplayNameEndpoint.cs
+++ src/BugHunters.Api/Features/ChangeDisplayName/ChangeDisplayNameEndpoint.cs
@@ -1,23 +1,40 @@
 ﻿using BugHunters.Api.Common.Endpoint;
 using BugHunters.Api.Common.HandlerContracts;
+using BugHunters.Api.Entities;
+using BugHunters.Api.Entities.Values.Hunter;
+using BugHunters.Api.Persistence;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BugHunters.Api.Features.ChangeDisplayName;
 
-public class ChangeDisplayNameEndpoint(ICommandHandler<ChangeDisplayNameCommand> handler)
+public class ChangeDisplayNameEndpoint(BugHunterContext context)
     : ApiEndpoint
-        .WithRequest<ChangeDisplayNameRequest>
-        .WithoutResponse
+        .WithRequest<ChangeDisplayNameEndpoint.ChangeDisplayNameRequest>
 {
     [HttpPost("update-displayname")]
-    public override async Task<ActionResult> HandleAsync([FromBody] ChangeDisplayNameRequest request)
-    {
-        ChangeDisplayNameCommand cmd = new(request.HunterId, request.NewDisplayName);
-        Result<None> result = await handler.HandleAsync(cmd);
-        return result.IsSuccess
-            ? Ok()
-            : BadRequest(result.Errors);
-    }
-}
+    public override async Task<IResult> HandleAsync([FromBody] ChangeDisplayNameRequest request) =>
+        await Id<Hunter>.FromString(request.HunterId)
+            .Bind(id => context.Hunters.SingleOrFailureAsync(h => h.Id == id))
+            .Bind(hunter => ChangeDisplayName(hunter, request.NewDisplayName))
+            .Map<Hunter, Hunter>(hunter => context.Hunters.Update(hunter).Entity)
+            .Tee(context.TrySaveChangesAsync)
+            .Match(
+                _ => Results.NoContent(),
+                ToProblemDetails
+            );
 
-public record ChangeDisplayNameRequest(string HunterId, string NewDisplayName);
+
+    public static Result<Hunter> ChangeDisplayName(Hunter hunter, string newDisplayName) =>
+        DisplayName.FromString(newDisplayName)
+            .Map(name => hunter with { DisplayName = name });
+
+    // {
+    //     
+    //     ChangeDisplayNameCommand cmd = new(request.HunterId, request.NewDisplayName);
+    //     Result<None> result = await handler.HandleAsync(cmd);
+    //     return result.IsSuccess
+    //         ? Ok()
+    //         : BadRequest(result.Errors);
+    // }
+    public record ChangeDisplayNameRequest(string HunterId, string NewDisplayName);
+}

a5fc340f696092984f55e68d654632e74070c93a
TRMO <tromortensen@hotmail.com>
2024-09-08 21:09:17 +0200

    refactor. SingleOrFailure => Any
    Took 8 minutes

--- src/BugHunters.Api/Features/CatchBug/CatchBugEndpoint.cs
+++ src/BugHunters.Api/Features/CatchBug/CatchBugEndpoint.cs
@@ -2,6 +2,7 @@
 using BugHunters.Api.Entities;
 using BugHunters.Api.Persistence;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using static BugHunters.Api.Common.Result.ResultExt;
 
 namespace BugHunters.Api.Features.CatchBug;
@@ -29,28 +30,24 @@ public class CatchBugEndpoint(BugHunterContext context)
         bugCatch => context.BugCatches.Add(bugCatch).Entity;
 
     private Func<HunterBugIds, Task<Result<HunterBugIds>>> BugExists() =>
-        ids => context.Bugs.SingleOrFailureAsync(b => b.Id == ids.BugId)
-            .Match(
-                _ => ids,
-                errors => Failure<HunterBugIds>(errors.ToArray())
-            );
+        async ids => await context.Bugs.AnyAsync(b => b.Id == ids.BugId)
+            ? Success(ids)
+            : Failure<HunterBugIds>(new ResultError("BugNotFound", "Bug not found."));
 
     private static Func<HunterBugIds, Task<Result<HunterBugIds>>> BugNotAlreadyCaught(BugHunterContext ctx) =>
-        ids => ctx.BugCatches.SingleOrFailureAsync(cb => cb.BugId == ids.BugId && cb.HunterId == ids.HunterId)
-            .Match(
-                _ => ids,
-                errors => Failure<HunterBugIds>(errors.ToArray())
-            );
+        async ids => await ctx.BugCatches.AnyAsync(cb => cb.BugId == ids.BugId && cb.HunterId == ids.HunterId)
+            ? Success(ids)
+            : Failure<HunterBugIds>(new ResultError("BugCatch", "Bug already caught."));
+
 
     private static BugCatch IdsToBugCatch(HunterBugIds tuple) =>
         new(tuple.HunterId, tuple.BugId, DateTime.Now);
 
     private static Func<HunterBugIds, Task<Result<HunterBugIds>>> HunterExists(BugHunterContext ctx) =>
-        ids => ctx.Hunters.SingleOrFailureAsync(h => h.Id == ids.HunterId)
-            .Match(
-                _ => ids,
-                errors => Failure<HunterBugIds>(errors.ToArray())
-            );
+        async ids => await ctx.Hunters.AnyAsync(h => h.Id == ids.HunterId)
+            ? Success(ids)
+            : Failure<HunterBugIds>(new ResultError("HunterNotFound", "Hunter not found."));
+
 
     private static Result<HunterBugIds> ToIdPairFrom(CatchBugRequest request) =>
         ValuesToObject(

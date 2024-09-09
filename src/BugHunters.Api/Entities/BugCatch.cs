a3eec78d365ba3c3119db7948366a73095183d06
TRMO <tromortensen@hotmail.com>
2024-09-09 10:41:36 +0200

    just a comment for future improvements
    Took 3 minutes

--- src/BugHunters.Api/Entities/BugCatch.cs
+++ src/BugHunters.Api/Entities/BugCatch.cs
@@ -5,6 +5,6 @@ namespace BugHunters.Api.Entities;
 
 public record BugCatch(Id<Hunter> HunterId, Id<Bug> BugId, DateTime TimeCaught)
 {
-    public Hunter Hunter { get; set; }
+    public Hunter Hunter { get; set; } // TODO should I try out Option<Hunter> here? Or just =null! that shit
     public Bug Bug { get; set; }
 }

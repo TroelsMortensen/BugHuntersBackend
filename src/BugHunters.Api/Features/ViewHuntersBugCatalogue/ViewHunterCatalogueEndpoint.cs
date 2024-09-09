34f92092a413c37351caf01d84b6c455cdba1c07
TRMO <tromortensen@hotmail.com>
2024-09-09 10:35:17 +0200

    Updated to railway
    Took 52 seconds

--- src/BugHunters.Api/Features/ViewHuntersBugCatalogue/ViewHunterCatalogueEndpoint.cs
+++ src/BugHunters.Api/Features/ViewHuntersBugCatalogue/ViewHunterCatalogueEndpoint.cs
@@ -1,26 +1,37 @@
 ﻿using BugHunters.Api.Common.Endpoint;
-using BugHunters.Api.Common.HandlerContracts;
+using BugHunters.Api.Entities.Common;
+using BugHunters.Api.Entities.HunterEntity;
+using BugHunters.Api.Persistence;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BugHunters.Api.Features.ViewHuntersBugCatalogue;
 
-public class ViewHunterCatalogueEndpoint(IQueryHandler<ViewCatalogueQuery, ViewCatalogueAnswer> handler)
-    : ApiEndpoint
-        .WithRequest<ViewCatalogueRequest>
-        .WithResponse<ViewCatalogueAnswer>
+public class ViewHunterCatalogueEndpoint(BugHunterContext context)
+    : ApiEndpoint.WithRequest<ViewHunterCatalogueEndpoint.ViewCatalogueRequest>
 {
-    [HttpGet("hunter/{HunterId}/catalogue")]
-    public override async Task<ActionResult<ViewCatalogueAnswer>> HandleAsync([FromRoute] ViewCatalogueRequest request)
-    {
-        Console.WriteLine("Request received: " + request.HunterId);
-        Result<ViewCatalogueAnswer> result = await handler.HandleAsync(new ViewCatalogueQuery(request.HunterId));
-        if (result.IsFailure)
-        {
-            return BadRequest(result.Errors);
-        }
+    [HttpGet("hunter-catalogue")]
+    public override async Task<IResult> HandleAsync([FromBody] ViewCatalogueRequest request) =>
+        await request.HunterId
+            .ToResult()
+            .Bind(Id<Hunter>.FromString)
+            .Where(id => context.HunterExists(id, id))
+            .Map(id => LoadCatches(id, context))
+            .Map(list => new ViewCatalogueResponse(list))
+            .Match(
+                Results.Ok,
+                ToProblemDetails);
 
-        return Ok(result.Payload);
-    }
+    private static Task<List<BugDto>> LoadCatches(Id<Hunter> id, BugHunterContext ctx) =>
+        ctx.BugCatches
+            .Where(b => b.HunterId == id)
+            .OrderBy(b => b.TimeCaught) // probably won't work because DateTime. Should use string? Should test...
+            .Select(b => new BugDto(b.Bug.Name, b.Bug.Description, b.Bug.Image))
+            .ToListAsync();
+
+    public record ViewCatalogueRequest(string HunterId);
+
+    private record ViewCatalogueResponse(List<BugDto> Bugs);
+
+    private record BugDto(string Name, string Description, byte[] Image);
 }
-
-public record ViewCatalogueRequest([FromRoute] string HunterId);
